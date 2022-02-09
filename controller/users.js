0e408f060748898e68e543c56d4a37b66576ebf5
sawairanawaz <sawaira.nawaz@ebitlogix.com>
2022-02-09 23:38:02 +0500

    Rest API's With Firestore Commpleted

---
+++ controller/users.js
@@ -0,0 +1,53 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { db } from "../firebase/firebase.js"
+import { collection, addDoc , getDocs, deleteDoc,updateDoc, doc} from "firebase/firestore";
+
+const userDb = collection(db,"students")
+
+
+export const getUsers = async(req, res) => {
+    const students = await getDocs(userDb);
+    const AllStudents = [];
+    students.docs.map((doc)=>(AllStudents.push({...doc.data(),id:doc.id})))
+    res.send(AllStudents);
+}
+
+export const createUser = async(req, res) => {
+
+    const student = req.body;
+    await addDoc(userDb,{ ...student});
+    res.send(`Student with the name ${user.FirstName} added to the database!`)
+}
+
+export const getUser = async(req, res) => {
+    const students = await getDocs(collection(db,"students"));
+    const AllStudents = [];
+    students.docs.map((doc)=>(AllStudents.push({...doc.data(),id:doc.id})))
+    const { id } = req.params;
+    const FoundUser = AllStudents.find((user) => user.id === id);
+    res.send(FoundUser);
+}
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const studentDoc = doc(db, "students", id);
+        await deleteDoc(studentDoc);
+    }
+    catch(err){
+        console.log(err);
+    }
+    finally{
+        res.send(`Student with id ${id} deleted from the database!`)
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const conDoc = doc(db, "students", id);
+    const { FirstName, LastName, Age } = req.body;
+    const newStatus = { FirstName:FirstName,LastName:LastName,Age:Age};
+    await updateDoc(conDoc, newStatus);
+    res.send(`user ${id} has been updated`)
+
+}
---
+++ firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTJ_0yVCw8ImqanQf780V4YXwFjWyS1CU",
+  authDomain: "crud-b8411.firebaseapp.com",
+  projectId: "crud-b8411",
+  storageBucket: "crud-b8411.appspot.com",
+  messagingSenderId: "596399381885",
+  appId: "1:596399381885:web:a770bbc7b348c3b839c014",
+  measurementId: "G-HV15VQ00EF"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
